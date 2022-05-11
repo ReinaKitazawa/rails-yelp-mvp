32915f4c89c66e874273d8d54fda9e07f5285361
Reina Kitazawa <kreina1015@yahoo.co.jp>
2022-05-11 15:58:36 +0900

    rails yelp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  category = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: category }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+yoshinoya = { name: "Yoshinoya", address: "Ebisu", category: "japanese"}
+jiro = { name: "Jiro", address: "Meguro", category: "french"}
+cocos = { name: "Cocos", address: "Shibuya", category: "italian"}
+sushiro = { name: "Sushiro", address: "Asakusa", category: "japanese"}
+osho = { name: "Osho", address: "Meguro", category: "chinese"}
+
+[yoshinoya, jiro, cocos, sushiro, osho].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
